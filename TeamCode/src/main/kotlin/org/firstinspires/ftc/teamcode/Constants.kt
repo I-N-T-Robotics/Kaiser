c56b513f243d103cd002d07d39bb78fa6b0a7946
Wing57 <ddariusd007@gmail.com>
2023-10-15 16:20:01 -0400

    adding full kt support

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/Constants.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/Constants.kt
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode
 
+import com.acmerobotics.roadrunner.MecanumKinematics
+
 class Constants {
     object Drive {
         //TODO: THIS IS FOR DYLANS ROBOT, CHANGE FOR KAISER
@@ -12,17 +14,18 @@ class Constants {
     }
 
     object Vision {
-        const val DESIRED_DISTANCE = 12.0;
-        const val DESIRED_TAG_ID = 4;
+        const val DESIRED_DISTANCE = 12.0
+        const val DESIRED_TAG_ID = 4
 
-        const val SPEED_GAIN =  0.03;
-        const val STRAFE_GAIN =  0.015;
-        const val TURN_GAIN =  0.04;
+        const val SPEED_GAIN =  0.03
+        const val STRAFE_GAIN =  0.015
+        const val TURN_GAIN =  0.04
 
-        const val MAX_AUTO_SPEED = 0.8;
-        const val MAX_AUTO_STRAFE = 0.7;
-        const val MAX_AUTO_TURN = 0.3;
+        const val MAX_AUTO_SPEED = 0.8
+        const val MAX_AUTO_STRAFE = 0.7
+        const val MAX_AUTO_TURN = 0.3
 
         const val USE_WEBCAM = true
+
     }
 }
