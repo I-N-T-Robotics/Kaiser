8787ef30053aaf1ed03a9a39fd347cf7fa1c8c0e
Wing57 <ddariusd007@gmail.com>
2023-10-19 17:56:59 -0400

    right arm inversions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ArmTest.java
@@ -3,7 +3,9 @@ package org.firstinspires.ftc.teamcode.Testing;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.Constants;
 
@@ -17,6 +19,11 @@ public class ArmTest extends OpMode {
     public void init() {
         LIFT_1 = hardwareMap.get(DcMotorEx.class, Constants.Lift.LIFT_1);
         LIFT_2 = hardwareMap.get(DcMotorEx.class, Constants.Lift.LIFT_2);
+
+        LIFT_2.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        LIFT_1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        LIFT_2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
@@ -26,5 +33,8 @@ public class ArmTest extends OpMode {
 
         LIFT_1.setPower(slidePower);
         LIFT_2.setPower(slidePower);
+
+        telemetry.addData("LIFT 1 TICKS:", LIFT_1.getCurrentPosition());
+        telemetry.addData("LIFT 2 TICKS:", LIFT_2.getCurrentPosition());
     }
 }
